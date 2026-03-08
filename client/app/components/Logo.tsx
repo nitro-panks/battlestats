2038146d20be1ef272af1c425d7100c1df334521
august schlubach <august.schlubach@gmail.com>
2026-03-08 19:29:55 -0400

    Fix client runtime and container setup

--- client/app/components/Logo.tsx
+++ client/app/components/Logo.tsx
@@ -1,21 +1,22 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 const Logo: React.FC = () => {
-    const handleClick = (e: React.MouseEvent) => {
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
         e.preventDefault();
         window.dispatchEvent(new CustomEvent("resetApp"));
     };
 
     return (
-        <a
+        <Link
             href="/"
             onClick={handleClick}
             className="text-xl font-bold tracking-tight text-[#084594] hover:text-[#2171b5] transition-colors"
         >
             WoWs Battlestats
-        </a>
+        </Link>
     );
 };
 
